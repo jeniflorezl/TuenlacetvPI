7e5780faffd36adccd8dbd54d299650ea1dc5169
Jeniffer Florez Londoño <jeniflorezl95@gmail.com>
2017-11-23 18:58:16 -0500

    changes

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,4 +1,6 @@
 class Zone < ApplicationRecord
     belongs_to :city
     has_many :neighborhoods
+    has_many :rates
+    has_many :concepts, :through => :rates
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :zones do
     resources :neighborhoods
+    resources :concetps do
+      resources :rates
+    end
   end
+  resources :concetps
   resources :companies
 end
