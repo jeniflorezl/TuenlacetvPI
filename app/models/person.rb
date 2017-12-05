dacb20809f58235396b0dfffd85f7a7fd5a121f6
Jeniffer Florez Londoño <jeniflorezl95@gmail.com>
2017-12-04 20:31:29 -0500

    changes

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,7 +3,6 @@ class Person < ApplicationRecord
   belongs_to :neighborhood
   belongs_to :zone
   has_many :entities
-  validates :type_document_id, :documento, :nombre1, :apellido1, :direccion, 
-  :neighborhood_id, :zone_id, :telefono1, :fechanac, :tipopersona, :estrato, 
-  :usuario, presence: true #obligatorio
+  validates :type_document_id, :documento, :nombre1, :apellido1, :direccion, :neighborhood_id, 
+  :zone_id, :telefono1, :fechanac, :tipopersona, :estrato, :usuario, presence: true #obligatorio
 end
